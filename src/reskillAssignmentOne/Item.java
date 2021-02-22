92661548b14e106a26c87110d07acaddcc7ee221
Svetlana <svetlana.haugland@gmail.com>
2021-02-22 17:27:23 +0100

    Har laget klasser

--- src/reskillAssignmentOne/Item.java
+++ src/reskillAssignmentOne/Item.java
@@ -1,6 +1,19 @@
 package reskillAssignmentOne;
 
+import lombok.Getter;
+
+@Getter
 public class Item {
     private String type;
-    private double unitTries;
+    private double unitPrice;
+    private double unitCost;
+    private int id;
+    private static int nextId = 0;
+
+    public Item(String type, double unitPrice, double unitCost){
+        id = nextId++;
+        this.type = type;
+        this.unitPrice = unitPrice;
+        this.unitCost = unitCost;
+    }
 }
--- src/reskillAssignmentOne/Order.java
+++ src/reskillAssignmentOne/Order.java
@@ -1,4 +1,22 @@
 package reskillAssignmentOne;
 
+import java.time.LocalDate;
+import java.util.HashMap;
+
 public class Order {
+    private long orderId;
+    private LocalDate orderDate;
+    private LocalDate shipDate;
+    private char orderPriority;
+    private HashMap<Item, Integer> itemsOnOrder;
+
+    public Order(long orderId, LocalDate orderDate, LocalDate shipDate, char orderPriority, Item item, int amountOfItems) {
+        this.orderId = orderId;
+        this.orderDate = orderDate;
+        this.shipDate = shipDate;
+        this.orderPriority = orderPriority;
+        itemsOnOrder = new HashMap<>();
+        itemsOnOrder.put(item, amountOfItems);
+
+    }
 }
