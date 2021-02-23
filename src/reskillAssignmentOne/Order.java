bb1f7c9e5824b7c7c2a41483911ae1cc4f6857e0
Svetlana <svetlana.haugland@gmail.com>
2021-02-23 15:13:32 +0100

    lagt inn kommentarer

--- src/reskillAssignmentOne/Order.java
+++ src/reskillAssignmentOne/Order.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+
 public class Order {
     private long orderId;
     private LocalDate orderDate;
@@ -11,22 +12,28 @@ public class Order {
     private char orderPriority;
     private String country;
     private String channel; // vurdere å gjøre det som enum
-    private HashMap<Item, Integer> itemsOnOrder;
+    private HashMap<Item, Integer> itemsOnOrder; //((HashMap (Item, Unitssolgt)))
     private ArrayList<Item> itemsonOrder;
 
+    double revenue;
+
+    public Order(Long orderId, double revenue){
+        this.orderId = orderId;
+        this.revenue=revenue;
+    }
     public Order(long orderId, LocalDate orderDate, LocalDate shipDate, char orderPriority, String itemType, int amountOfItems, String region, String country) {
         this.orderId = orderId;
         this.orderDate = orderDate;
         this.shipDate = shipDate;
         this.orderPriority = orderPriority;
-        this.region = region;
         this.country = country;
         itemsOnOrder = new HashMap<>();
-        itemsOnOrder.put(itemType, amountOfItems);
+
 
     }
 
     public double getRevenuePerOrder(){
+        return revenue;
 
 
     }
