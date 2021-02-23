b70e358ed9c3f7041ec2697ba5875ac2311b3890
Tom Forsell <77614564+TomForsell@users.noreply.github.com>
2021-02-23 09:14:54 +0100

    More tests added to CSVReader

--- src/reskillAssignmentOne/CSVFileHandling.java
+++ src/reskillAssignmentOne/CSVFileHandling.java
@@ -1,20 +1,15 @@
 
 package reskillAssignmentOne;
 
-/*import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
-import com.opencsv.exceptions.CsvException;
-*/
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CSVFileHandling {
-    private String filePath= "C:\\Reskill\\salesrecords.txt";
+    //private String filePath= "C:\\Reskill\\salesrecords.txt";
     private  String line="";
     private  String[] csvValues;
     private List<String[]> orderlist = new ArrayList<>();
-   
     public List readCSVFile (String filePath) throws FileNotFoundException,IOException{
 
         BufferedReader br;
@@ -34,26 +29,4 @@ public class CSVFileHandling {
         }
     return orderlist;
     }
-
-    /*
-    public  void readAllDataAtOnce()
-    {
-        try {
-            CSVReader reader = new CSVReader(new FileReader("C:\\Reskill\\salesrecords.txt"));
-            String[] nextLine;
-            while ((nextLine = reader.readNext()) != null) {
-                if (nextLine != null) {
-                    System.out.println(Arrays.toString(nextLine));
-                }
-
-            }
-        }catch (Exception e){
-            System.out.println(e);
-
-        }
-    }
-
-     */
-
-
 }
--- src/reskillAssignmentOne/CSVFileHandlingTest.java
+++ src/reskillAssignmentOne/CSVFileHandlingTest.java
@@ -12,18 +12,56 @@ import java.util.List;
 
 public class CSVFileHandlingTest {
 
-    private String filePath= "C:\\Reskill\\salesrecords.txt";
+    private String filePath;
+    private String emptyFile;
+    private String oneLineCorrectFormat;
     private CSVFileHandling testCSVReader;
     private List<String[]> testOrderList;
-    private String[] line713 = {"Sub-Saharan Africa","Niger","Clothes","Offline","M","1/10/2012","201730287","2/19/2012","5330","109.28","35.84","582462.40","191027.20","391435.20"};;
+    private final String[] line713 = {"Sub-Saharan Africa","Niger","Clothes","Offline","M","1/10/2012","201730287","2/19/2012","5330","109.28","35.84","582462.40","191027.20","391435.20"};;
 
+/*•
+•	Try to open a file with 1 record – should report 1 record found
+•	Try to open a file with 1 record with correct format – should be able to parse line correctly and return correctly structured object
+•	Try to open a file with 1 record with incorrect format (several possibilities here) – should report error
+•	Try to open a file with several records, all correct format – should be able to parse lines correctly and return collection of correctly structured objects
+•	Try to open a file with several records, some valid, some invalid – you should decide for yourself what you expect to happen here (e.g. total failure, or partial success)
 
+*/
     @Before
     public void setUp() throws Exception {
-         String filePath= "C:\\Reskill\\salesrecords.txt";
+         filePath= "C:\\Reskill\\salesrecords.txt";
+        emptyFile = "C:\\Reskill\\emptyFile.txt";
+        oneLineCorrectFormat = "C:\\Reskill\\oneLineCorrectFormat.txt";
+
          testCSVReader = new CSVFileHandling();
          testOrderList = new ArrayList<>();
     }
+    @Test
+    public void testEmptyFile() throws FileNotFoundException, IOException
+    {
+        testOrderList=testCSVReader.readCSVFile(emptyFile);
+        assertEquals(0,testOrderList.size());
+    }
+
+    @Test
+    public void testReportOneRecord()throws FileNotFoundException, IOException
+    {
+        testOrderList=testCSVReader.readCSVFile(oneLineCorrectFormat);
+        assertEquals(1,testOrderList.size());
+    }
+
+    @Test
+    public void testOneLineCorrectFormat()throws FileNotFoundException, IOException
+    {
+        testOrderList=testCSVReader.readCSVFile(oneLineCorrectFormat);
+        String[] actualStringArray = testOrderList.get(0);
+        int n =1;
+        for(int i=0;i<actualStringArray.length;i++) {
+            assertEquals(actualStringArray[i], "test"+n);
+            n++;
+        }
+    }
+
     @Test
     public void testNumberofLinesInCSV() throws FileNotFoundException, IOException {
         testOrderList=testCSVReader.readCSVFile(filePath);
